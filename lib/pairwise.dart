fcd32bd28df1615ca2280544e68f104d030d50e0
Laurent Couturier <laurent_couturier@connect-tech.sncf>
2026-02-11 13:56:27 +0100

    refactor: :recycle: rework pairwise iterator

--- lib/pairwise.dart
+++ lib/pairwise.dart
@@ -1,17 +1,25 @@
 class PairwiseIterator<T> implements Iterator<({T left, T right})> {
-  final Iterator<T> left;
-  final Iterator<T> right;
+  final Iterable<T> items;
+  int _index;
 
-  PairwiseIterator(this.left, this.right);
+  PairwiseIterator(this.items) : _index = 0;
+
+  ({T left, T right})? _current;
 
   @override
-  ({T left, T right}) get current => (left: left.current, right: right.current);
+  ({T left, T right}) get current => _current!;
 
   @override
   bool moveNext() {
-    if (!left.moveNext() || !right.moveNext()) return false;
+    final pair = items.skip(_index).take(2);
 
-    return true;
+    if ((pair.isNotEmpty) && (pair.length == 2)) {
+      _current = (left: pair.first, right: pair.last);
+      _index++;
+      return true;
+    }
+
+    return false;
   }
 }
 
@@ -19,10 +27,13 @@ class PairwiseIterable<T> extends Iterable<({T left, T right})> {
   final Iterable<T> items;
   PairwiseIterable(this.items);
   @override
-  Iterator<({T left, T right})> get iterator =>
-      PairwiseIterator(items.iterator, items.skip(1).iterator);
+  Iterator<({T left, T right})> get iterator => PairwiseIterator(items);
 }
 
 extension PairwiseExtension<T> on Iterable<T> {
+  /// Pairwise
+  /// returns an iterable of pairs
+  /// example: [1,2,3,4,5].pairwise()
+  /// returns [(1,2), (2,3), (3,4), (4,5)]
   Iterable<({T left, T right})> pairwise() => PairwiseIterable(this);
 }
